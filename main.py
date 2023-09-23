b495249e9979343ba52128d5757797486dce7351
mahendra <mkjagtap@gmail.com>
2023-09-23 20:46:10 +0530

    Added main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from bicycle import Bicycle
+
+print(f"inside home function")
+print(f"inside new home function")
+
+app = FastAPI()
+bicycle_list = []
+
+
+@app.get("/")
+def home():
+    print(f"inside home function")
+    return{"message": "Welcome to the Mukesh's bicycle shop!"}
+
+@app.post("/")
+def add_new_bicycle(bicycle: Bicycle):
+    print(f"Inside add new bicycle method")
+    return bicycle_list
+
+@app.get("/bicycle")
+def get_bicycle():
+    print(f"inside get bicycle")
+    return {"available_bicycle": bicycle_list}
